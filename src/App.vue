f6ed7158dc0634d0463cbe40b87264315ec69a45
kevin.liang梁言成 <kevin.liang@awan-ant.com>
2025-03-24 11:12:33 +0800

    feat(ui): add Todo header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <!-- src/App.vue -->
 <template>
-  <div>
-    <h1>todo</h1>
+  <h1>Vue.js Todo list</h1>
+
+  <div class="todo-container">
+    <AppHeader
+      :user="user"
+      @logout="logout"
+    />
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import AppHeader from './components/AppHeader.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AppHeader
+  },
+  setup() {
+    const user = ref({
+      name: '測試用戶',
+      level: '一般會員'
+    })
+
+    function logout() {
+      alert('登出(展示用)')
+    }
+
+    return {
+      user,
+      logout
+    }
+  }
 };
 </script>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="app-header">
+        <h2>welcome, {{ user.name }} ({{ user.level }})</h2>
+        <button @click="$emit('logout')" class="logout-button">登出</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader',
+        props: {
+            user: {
+                type: Object,
+                required: true
+            }
+        },
+        emits: ['logout']
+    }
+</script>
+
+<style scoped>
+    .app-header {
+        margin-bottom: 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    h2 {
+        margin:  0;
+    }
+
+    .logout-button {
+        background-color: #ff4949;
+        color: white;
+        border: none;
+        padding: 5px 10px;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+</style>
